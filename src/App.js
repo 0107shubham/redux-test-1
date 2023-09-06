59d73c603cf28023d5a5518a1ba56cf2ccb9591b
shubham tyagi <120669180+0107shubham@users.noreply.github.com>
2023-09-06 18:14:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import Counter from "./component/Counter";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>shubham</h1>
+      <Counter />
+    </div>
+  );
+}
---
+++ src/container/configureStore.js
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../component/counterSlice";
+export default configureStore({
+  reducer: { counter: counterReducer }
+});
